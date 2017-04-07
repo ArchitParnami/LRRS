088c028553fddb85c5f822acf60bc7a4300a4246
mjq8708 <mjqseu@gmail.com>
2017-04-07 17:28:37 -0400

    Add files via upload

--- login.py
+++ login.py
@@ -1,18 +1,34 @@
 from flask import Flask, redirect, url_for, request,json
 #render_template is for render html to your server
-import lrrs
-
 from flask import render_template, jsonify
+from flask import session
+import os
 # sql connector
+from flaskext.mysql import MySQL
 
-from App import  mysql
-from App import  app
 
+app = Flask(__name__)
+
+# # MySQL configurations
+# mysql = MySQL()
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
+# app.config['MYSQL_DATABASE_DB'] = 'lrrs'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# mysql.init_app(app)
+
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
+app.config['MYSQL_DATABASE_DB'] = 'lrrs'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 def queryuser(username,password):
     conn = mysql.connect()
     cursor = conn.cursor()
-    query_str = "SELECT * FROM user_info WHERE uname='%s' AND password='%s';"%(username, password)
+    query_str = "SELECT * FROM user_pw WHERE username='%s' AND password='%s';"%(username, password)
     cursor.execute(query_str)
     return_data = cursor.fetchall()
     cursor.close()
@@ -37,8 +53,12 @@ def checkuser(_uname,_password):
         status = {'success':1}
     return status
 
+@app.route('/')
 def login():
-    return render_template("login.html")
+    if not session.get('logged_in'):
+        return render_template("login.html")
+    else:
+        return "You have already logged in.  Click <a href='/searchpage.html'>here</a> to search rooms."
 
 
 @app.route('/checkuname.xyz',methods = ['POST'])
@@ -46,20 +66,23 @@ def checkuname():
     # read the posted values from the UI
     uname = request.form['username']
     password = request.form['psw']
+    if checkuser(uname,password)=={'success':1}:
+        print('assert true1')
+        session['logged_in'] = True
+        session[uname]=password
+        print('assert true2')
     return jsonify(checkuser(uname,password))
     
 
 @app.route('/searchpage.html')
 def searchpage():
-
-    return render_template("searchpage.html")
-
-
-
-@app.route("/")
-def start():
-    return login()
-
+    # if session.get('logged_in'):
+    if session.get('logged_in'):
+        return render_template("searchpage.html")
+    else:
+        return render_template("pleaseloginfirst.html")
+    
 
 if __name__ == '__main__':
+   app.secret_key = os.urandom(12)
    app.run(debug = True)
