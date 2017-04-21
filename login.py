25ffc06f6acf53d7d5f344a0a54794b2b6422d32
mjq8708 <mjqseu@gmail.com>
2017-04-21 17:33:47 -0400

    Add files via upload

--- login.py
+++ login.py
@@ -1,7 +1,16 @@
-from flask import Flask, redirect, url_for, request,json
+from flask import Flask, redirect, url_for, request, json, Response, abort
 #render_template is for render html to your server
 from flask import render_template, jsonify
-from flask import session
+
+from flask_login import LoginManager, UserMixin, \
+                                login_required, login_user, logout_user 
+
+import random
+random.seed(1234)
+
+import logging
+from logging.handlers import RotatingFileHandler
+
 import os
 # sql connector
 from flaskext.mysql import MySQL
@@ -9,15 +18,12 @@ from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
-# # MySQL configurations
-# mysql = MySQL()
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
-# app.config['MYSQL_DATABASE_DB'] = 'lrrs'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# mysql.init_app(app)
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 
-
+# MySQL configurations
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
@@ -25,64 +31,226 @@ app.config['MYSQL_DATABASE_DB'] = 'lrrs'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-def queryuser(username,password):
+def user_password_check(username, password):
     conn = mysql.connect()
     cursor = conn.cursor()
-    query_str = "SELECT * FROM user_pw WHERE username='%s' AND password='%s';"%(username, password)
+    query_str = "SELECT password FROM user_pw WHERE username='%s';"%(username)
     cursor.execute(query_str)
-    return_data = cursor.fetchall()
+    saved_password = cursor.fetchall()
     cursor.close()
     conn.close()
-    return return_data
-
-def checkuser(_uname,_password):
-
-    print(_uname)
-    print(_password)
-
-    res_data = queryuser(_uname,_password)
-    
-    print('return type is list')
-    print(res_data)
-    # return json.dumps({res_data[0][0]:res_data[0][1]})
-    if len(res_data)==0 or res_data is None:
-        print ('not found')
-        status = {'success':0}
+    if saved_password is None or len(saved_password) < 1 or saved_password[0][0]!= password:
+        return False
     else:
-        print( 'found')
-        status = {'success':1}
-    return status
+        return True
 
-@app.route('/')
+# silly user model
+class User(UserMixin):
+
+    def __init__(self, id, active=True):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = self.name + "_password"
+        self.active = active
+        
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+    def is_active(self):
+        # Here you should write whatever the code is
+        # that checks the database if your user is active
+        return self.active
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
+
+# callback to reload the user object        
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+
+@app.route("/")
+def root():
+    return "Click <a href='/searchpage.html'>here</a> to search rooms."
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if not session.get('logged_in'):
-        return render_template("login.html")
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print('username:password is %s:%s'%(username, password))        
+        if user_password_check(username, password):
+            id = random.randrange(1,9999)
+            user = User(id)
+            login_user(user)
+            app.logger.info('Info: User %s logged in'%(username))
+            return redirect(request.args.get("next"))
+        else:
+            return abort(401)
     else:
-        return "You have already logged in.  Click <a href='/searchpage.html'>here</a> to search rooms."
+        for arg in request.args:
+            print('key:value = %s:%s'%(arg , request.args[arg] ))
+        return Response('''
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title>login page</title>
+        <h2>Please enter your username and password here:</h2>
+        <style type="text/css">
+		        h2{
+			text-align: left;
+			font-size: 24px;
+		}
+
+		hr{
+			margin-bottom: 30px;
+		}
+
+		div.container{
+			width: 960px;
+			height: 610px;
+			margin:50px auto;
+			font-family: 'Droid Serif', serif;
+			position:relative;
+		}
+
+		div.main{
+			width: 320px;
+			margin-top: 80px;
+			float:left;
+			padding: 10px 55px 40px;
+			background-color: rgba(187, 255, 184, 0.65);
+			border: 15px solid white;
+			box-shadow: 0 0 10px;
+			border-radius: 2px;
+			font-size: 13px;
+		}
+
+		input[type=text],[type=password] {
+			width: 30%;
+			height: 34px;
+			padding-left: 5px;
+			margin-bottom: 20px;
+			margin-top: 8px;
+			box-shadow: 0 0 5px #00F5FF;
+			border: 2px solid #00F5FF;
+			color: #4f4f4f;
+			font-size: 16px;
+		}
+
+		label{
+			color: #464646;
+			text-shadow: 0 1px 0 #fff;
+			font-size: 14px;
+			font-weight: bold;
+		}
+
+		#login {
+			width: 30%;
+			background: linear-gradient(#22abe9 5%, #36caf0 100%);
+			border: 1px solid #0F799E;
+			font-size: 20px;
+			margin-top: 15px;
+			padding: 8px;
+			font-weight: bold;
+			cursor: pointer;
+			color: white;
+			text-shadow: 0px 1px 0px #13506D;
+		}
+
+		#login:hover{
+		    background: linear-gradient(#36caf0 5%, #22abe9 100%);
+		}
+
+		/* -------------------------------------
+		    CSS for sidebar (optional) 
+		---------------------------------------- */
+		.fugo{
+			float:right;
+		}
+        </style>
+        </head>	
+        <body>
+        <br/>
+        <form action="" method="post">
+            <label for="username">Username:</label>
+            <input type="text" name="username"  required="required"><br/>
+            <label for="password">Password:</label>
+            <input type="password" name="password" required="required"><br/>
+            <input type="submit" value="Login">
+        </form>
+        <body>
+        </html>
+        ''')
 
 
-@app.route('/checkuname.xyz',methods = ['POST'])
-def checkuname():
-    # read the posted values from the UI
-    uname = request.form['username']
-    password = request.form['psw']
-    if checkuser(uname,password)=={'success':1}:
-        print('assert true1')
-        session['logged_in'] = True
-        session[uname]=password
-        print('assert true2')
-    return jsonify(checkuser(uname,password))
+# logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return Response('<p>You have logged out</p>')
+
+
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('''
+                    <!DOCTYPE html>
+                    <html>
+                    <head>
+                    <style>
+                    .alert {
+                        padding: 20px;
+                        background-color: #f44336;
+                        color: white;
+                    }
+
+                    .closebtn {
+                        margin-left: 15px;
+                        color: white;
+                        font-weight: bold;
+                        float: right;
+                        font-size: 22px;
+                        line-height: 20px;
+                        cursor: pointer;
+                        transition: 0.3s;
+                    }
+
+                    .closebtn:hover {
+                        color: black;
+                    }
+                    </style>
+                    </head>
+                    <body>
+
+                    
+
+                    <div class="alert">
+                      <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+                      <strong>Login failed!</strong> Incorret username or password.
+                    </div>
+
+                    <p>Click <a href='/login?next=%2Fsearchpage.html'>here</a> to try again</p>
+                    </body>
+                    </html>
+
+        ''')
+    
     
 
 @app.route('/searchpage.html')
+@login_required
 def searchpage():
-    # if session.get('logged_in'):
-    if session.get('logged_in'):
-        return render_template("searchpage.html")
-    else:
-        return render_template("pleaseloginfirst.html")
-    
+    return render_template("searchpage.html")
 
+    
 if __name__ == '__main__':
-   app.secret_key = os.urandom(12)
-   app.run(debug = True)
+    handler = RotatingFileHandler('log.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
+    app.secret_key = os.urandom(12)
+    app.run(debug = True)
